57bcebcee467240651ebc1d6802b34a0272453b8
Krzysztof Wisniewski <m1a1osowa@gmail.com>
2022-11-04 13:09:55 +0100

    1

--- AoC2018/AoC2018.cpp
+++ AoC2018/AoC2018.cpp
@@ -8,27 +8,45 @@ int main()
 {
 	int frequency = 0;
 	int number;
+	int *tab;
+	int i = 0;
 	string mark;
 	string line;
 	ifstream file;
+
+	tab = new int[1000];
 
 	file.open("Dane1.txt", ios::in);
 
 	if (!file.good())
 		cout << "File do not exist";
 
-	while (getline(file, line))
+	while (true)
 	{
-		mark = line.substr(0, 1);
-		number = stoi(line.substr(1, line.length()));
-		if (mark == "+")
+		while (getline(file, line))
 		{
-			frequency = frequency + number;
+			cout << "line: " << line << endl;
+			mark = line.substr(0, 1);
+			number = stoi(line.substr(1, line.length()));
+			if (mark == "+")
+			{
+				frequency = frequency + number;
+
+			}
+			else
+			{
+				frequency = frequency - number;
+			}
+
+			for (int x = 0; x < i; x++)
+			{
+
+			}
+			i++;
 		}
-		else
-		{
-			frequency = frequency - number;
-		}
+		file.clear();
+		file.seekg(0);
 	}
+	
 	cout << "frequency: " << frequency << endl;
 }
